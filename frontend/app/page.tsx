29d09496cdb3790f5a76073a5b7c4f0301614e96
mjdierkes <mjdierkes@msn.com>
2025-04-16 10:42:53 -0700

    base

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle
+} from "@/components/ui/resizable";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="h-screen p-6">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="min-h-[400px] rounded-lg border"
+      >
+        <ResizablePanel defaultSize={50}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold"></span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={50}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold"></span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
