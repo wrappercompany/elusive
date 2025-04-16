31027c08a33cd5f1af564a9e8cbd3e708859ffd3
mjdierkes <mjdierkes@msn.com>
2025-04-16 13:52:09 -0700

    preview

--- frontend/components/Previews.tsx
+++ frontend/components/Previews.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export function Previews() {
   return (
-    <div className="h-full w-full flex items-center justify-center p-4">
-      <div className="w-full" style={{ aspectRatio: '19.5/9' }}>
+    <div className="h-full w-full flex items-center justify-center p-4 bg-[#fafafafa]">
+      <div className="mx-auto" style={{ width: '320px', height: '693px' }}>
         <iframe 
           src="http://localhost:19006/" 
-          className="w-full h-full border-0 rounded-md"
+          width="320"
+          height="693"
+          className="border-0 rounded-md"
           title="Preview"
         />
       </div>
