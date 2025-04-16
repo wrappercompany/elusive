55f0f6165104331b8f2c205f0d2a8d7ac827d8d8
mjdierkes <mjdierkes@msn.com>
2025-04-16 13:43:34 -0700

    update

--- frontend/components/Previews.tsx
+++ frontend/components/Previews.tsx
@@ -5,7 +5,7 @@ export function Previews() {
     <div className="h-full w-full flex items-center justify-center p-4">
       <div className="w-full" style={{ aspectRatio: '19.5/9' }}>
         <iframe 
-          src="http://localhost:8081" 
+          src="http://localhost:19006/" 
           className="w-full h-full border-0 rounded-md"
           title="Preview"
         />
